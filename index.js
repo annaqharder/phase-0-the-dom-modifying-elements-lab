808a6a83dd00e251674e5bc2fd32e60f7dec158c
Anna Harder <annaqharder@gmail.com>
2022-10-14 08:24:15 -0600

    commit tree aefb55dfc61dc438f85998595e21f7480bae1cc2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+main.remove(); 
+
+const newHeader = document.createElement("h1"); 
+// document.body.append(newHeader); 
+
+// newHeader.append('victory'); // does NOT create the id="victory" //
+
+// newHeader.id = 'victory'; // does create the id="victory" //
+
+newHeader.setAttribute('id', 'victory'); // does create the id="victory" //
+
+newHeader.textContent = "Anna is the champion"; 
